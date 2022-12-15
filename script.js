fbb1eeaf3b8d3e61e434d1f823c5b6128c825428
LumiNovryM <luminovrymekel@gmail.com>
2022-12-15 21:47:32 +0700

    Finishing

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+// Event
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('slide');
+});
